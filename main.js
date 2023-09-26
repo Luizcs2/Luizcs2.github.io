22f739476fd81c7b550a19da1a6fa5c74082a3ed
Luizcs2 <145772061+Luizcs2@users.noreply.github.com>
2023-09-26 15:06:31 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import "/style.css";
+
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.133.0/build/three.module.js';
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.133.0/examples/jsm/controls/OrbitControls.js';
 
